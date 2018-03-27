a8116c403b65634a7cb931d5ff9ba3ae1a9080cd
Darryll DeCoster <darryll@puppet.com>
2018-03-27 15:37:42 -0700

    Update config.php

--- api/php/config.php
+++ api/php/config.php
@@ -4,12 +4,12 @@
   */
 
   // Consumer Key
-  define('CONSUMER_KEY', 'MFRLjKoEJA0nkpvO0ablYtFPE');
-  define('CONSUMER_SECRET', 'f9S4x3cGQhSmG67MpJFmX0e7Dd2w5rKZxgJ9Q4B8mndXdZkBV5');
+  define('CONSUMER_KEY', '');
+  define('CONSUMER_SECRET', '');
 
   // User Access Token
-  define('ACCESS_TOKEN', '19018979-4VYEpKQHDQ49lXS0OfBTu6qM9WB2SdBgy6UjkTXkR');
-  define('ACCESS_SECRET', 'GGUGP7ac9ddcEKnCitUc0e1gqxraZ5B5SCQXRZLETdV2r');
+  define('ACCESS_TOKEN', '');
+  define('ACCESS_SECRET', '');
 
   // Cache Settings
   define('CACHE_ENABLED', false);

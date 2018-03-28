015e53162ff0926472b38f62cb31a015aa56bfef
Darryll DeCoster <darryll@puppet.com>
2018-03-27 17:21:47 -0700

    Update config.php

--- api/php/config.php
+++ api/php/config.php
@@ -3,13 +3,19 @@
   * Tweetie Config
   */
 
+  // fetch the keys from the settings file
+  $c_key = \Drupal\Core\Site\Settings::get('consumer_key');
+  $c_secret = \Drupal\Core\Site\Settings::get('consumer_secret');
+  $a_token = \Drupal\Core\Site\Settings::get('access_token');
+  $a_secret = \Drupal\Core\Site\Settings::get('access_secret')
+    
   // Consumer Key
-  define('CONSUMER_KEY', '');
-  define('CONSUMER_SECRET', '');
+  define('CONSUMER_KEY', $c_key);
+  define('CONSUMER_SECRET', $c_secret);
 
   // User Access Token
-  define('ACCESS_TOKEN', '');
-  define('ACCESS_SECRET', '');
+  define('ACCESS_TOKEN', $a_token);
+  define('ACCESS_SECRET', $a_secret);
 
   // Cache Settings
   define('CACHE_ENABLED', false);

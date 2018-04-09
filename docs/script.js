0c3638fd5973ff8e9f32a109e91571c5b035fd63
Darryll DeCoster <darryll@puppet.com>
2018-04-09 16:30:04 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $('#feed').tweetie({
-      "url": "api",
+      "url": "api/php/server.php",
       "type": "timeline",
       "template": "<li>{{tweet.created_at}} - {{tweet.text}}</li>",
       "dateFormat": "%b %d, %Y",
